d5198192dbb121fd27c323d866de3fa948a46e48
ThushanthSbm <thushanthsbm1997@gmail.com>
2020-03-27 19:51:01 +0530

    tute3

---
+++ app/src/main/java/com/example/androidfragment/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.androidfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.Fragment;
+
+import androidx.fragment.app.FragmentManager;
+
+import androidx.fragment.app.FragmentTransaction;
+
+
+
+import android.os.Bundle;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.fragment.app.Fragment;
+
+import androidx.fragment.app.FragmentManager;
+
+import androidx.fragment.app.FragmentTransaction;
+
+
+
+import android.os.Bundle;
+
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view){
+        Fragment fragment;
+        if (view == findViewById(R.id.btnFragment1)){
+            fragment = new fragment1();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fgmntDefault,fragment);
+            ft.commit();
+        }
+        if (view == findViewById(R.id.btnFragmetn2)){
+            fragment = new fragment2();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frgmntDefault,fragment);
+            ft.commit();
+        }
+    }
+
+}
